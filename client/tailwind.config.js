622379ec349615f3b91faf28f74870ae586f5b00
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-03-28 20:29:09 +0300

    build: remove boilerplate code

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        rubik: ['Rubik', 'sans-serif'],
+        poly: ['Poly', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 };
