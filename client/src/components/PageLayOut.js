f7f4668fd2fcfb1699bc58d7c57a5083ea84a417
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-03-30 16:14:22 +0300

    creating about component

--- client/src/components/PageLayOut.js
+++ client/src/components/PageLayOut.js
@@ -1,9 +1,12 @@
 import Header from "./Header"
+import React from 'react'
+import About from './About'
 
 const PageLayOut = () => {
   return (
     <div>
       <Header />
+      <About/>
     </div>
   )
 }
