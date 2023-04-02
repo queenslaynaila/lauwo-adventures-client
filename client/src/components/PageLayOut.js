fe30c9b7f127d862dc13afea55cd3caf3b8d96ed
queenslay jema <queenslayjema@gmail.com>
2023-04-02 15:44:35 +0300

    Feat:Import Popular section and inquiry form

--- client/src/components/PageLayOut.js
+++ client/src/components/PageLayOut.js
@@ -1,11 +1,14 @@
 import Header from "./Header"
 import About from './About'
-
+import PopularAdventure from "./PopularAdventure"
+import InquiryForm from "./InquiryForm"
 const PageLayOut = () => {
   return (
     <div>
       <Header />
       <About/>
+      <PopularAdventure/>
+      <InquiryForm/>
     </div>
   )
 }
