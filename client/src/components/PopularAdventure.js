d5b4e4cbc3647a8ae45688bdae897d8a56d1a031
queenslay jema <queenslayjema@gmail.com>
2023-04-03 00:38:24 +0300

    Fix:Increase iimage width on mobile

--- client/src/components/PopularAdventure.js
+++ client/src/components/PopularAdventure.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import {AiOutlineCompass } from 'react-icons/ai';
 const PopularAdventure = () => {
   return (
-    <section className='popular font-poly ' >
+    <section className='popular font-poly mt-4 ' >
        <div className='mx-50'>
          <div className="flex justify-center items-center">
              <div className="border-t border-black w-1/3"></div>
@@ -12,29 +12,22 @@ const PopularAdventure = () => {
              <div className="border-t border-black w-1/3"></div>
           </div>
         </div>
-        <h1 className="lg:text-4xl sm:text-lg font-bold text-center lg:my-4 sm:my-2">Popular Expeditures</h1>
-        <div className="grid grid-cols-3 gap-x-6 mx-20">
-
-          <div >
-              <div className="rounded-lg overflow-hidden mb-6 lg:h-80 sm:h-60">
-                <Image src="/safari-1.jpg" alt='nnn' height={500} width={500} className="w-full h-full object-cover image" />
-              </div>
-               <h3 className="sm:text-sm lg:text-2xl font-bold mb-3">Serengeti</h3>
-          </div>
-
-          <div>
-            <div className="shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover mb-6 lg:h-80 sm:h-60">
-                <Image src="/kilimanjaro.jpg" alt='nnn'  priority={true} height={500} width={500} className="w-full h-full object-cover image" /> 
+        <h1 className="font-bold text-center my-2 text-lg lg:text-3xl">Popular Expeditures</h1>
+        <div className="grid grid-cols-3 gap-x-6 mx-4 sm:mx-20 ">
+            <div className=' rounded-lg h-32 sm:h-60 md:h-50' > 
+                <Image src="/safari-1.jpg" alt='nnn' height={500} width={500} className="w-full  h-full object-cover  rounded-lg"/> 
+                <h3 className="sm:text-sm lg:text-2xl font-bold mb-3">Serengeti</h3>
             </div>
-            <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3">Kilimanjaro</h3>
-          </div>
 
-          <div>
-            <div className="shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover mb-6 lg:h-80 sm:h-60">
-              <Image src="/tarangire.jpg" alt='nnn' height={500} width={500} className="w-full  h-full object-cover image"/>
+            <div className='rounded-lg h-32 sm:h-60 md:h-50'>
+                <Image src="/kilimanjaro.jpg" alt='nnn'  priority={true} height={500} width={500} className="w-full h-full object-cover  rounded-lg" />  
+                <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3">Kilimanjaro</h3>
             </div>
-            <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3">Tarangire</h3>
-          </div>
+
+            <div className=' rounded-lg h-32 sm:h-60 md:h-50'>
+                <Image src="/tarangire.jpg" alt='nnn' height={500} width={500} className="w-full  h-full object-cover  rounded-lg"/> 
+                <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3">Tarangire</h3>
+            </div>
         </div>
       </section>
   )
