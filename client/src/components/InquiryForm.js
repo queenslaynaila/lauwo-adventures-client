93067bd65d0820754edc8797d29433c11e5107ce
queenslay jema <queenslayjema@gmail.com>
2023-04-02 15:04:49 +0300

    Fix:Remove empty placeholder

--- client/src/components/InquiryForm.js
+++ client/src/components/InquiryForm.js
@@ -9,16 +9,16 @@ const InquiryForm = () => {
                 <form className='mb-4'>
                     <div className="grid grid-cols-2 gap-6 my-4">
                       <div className="relative z-0 w-full mb-6 group">
-                          <input type="text" name="name" id="name" className="block py-2.5 px-0 w-full  lg:text-2xl sm:text-sm  text-black bg-transparent  border-b-2 border-black appearance-none focus:outline-none " placeholder=" " required />
+                          <input type="text" name="name" id="name" className="block py-2.5 px-0 w-full  lg:text-2xl sm:text-sm  text-black bg-transparent  border-b-2 border-black appearance-none focus:outline-none " required />
                           <label htmlFor="name" className="lg:text-2xl sm:text-sm absolute text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 font-bold">Name</label>
                       </div>
                     <div className="relative z-0 w-full mb-6 group">
-                          <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full  text-black bg-transparent  border-b-2 lg:text-2xl sm:text-sm  border-black appearance-none focus:outline-none" placeholder required />
+                          <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full  text-black bg-transparent  border-b-2 lg:text-2xl sm:text-sm  border-black appearance-none focus:outline-none" required />
                           <label htmlFor="email" className="peer-focus:font-medium absolute  text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0  font-bold lg:text-2xl sm:text-sm ">Email</label>
                     </div>
                     </div>
                     <div className="relative z-0 w-full mb-6 group">
-                        <input type="text" name="message" id="message" className="block py-2.5 px-0 w-full  text-gray-900 bg-transparent  border-b-2 border-black appearance-none lg:text-2xl sm:text-sm  focus:outline-none " placeholder=" " required />
+                        <input type="text" name="message" id="message" className="block py-2.5 px-0 w-full  text-gray-900 bg-transparent  border-b-2 border-black appearance-none lg:text-2xl sm:text-sm  focus:outline-none "  required />
                         <label htmlFor="message" className="peer-focus:font-medium absolute text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 font-bold lg:text-2xl sm:text-sm ">Message</label>
                     </div>
                     <button className="flex items-center mx-auto bg-yellow-500 font-medium rounded-lg px-5 py-2.5 text-center font-semibold">
