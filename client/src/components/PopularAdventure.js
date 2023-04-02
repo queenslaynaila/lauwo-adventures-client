07e28e52647bfa5b8049539cdc8fd31713c4c8b5
queenslay jema <queenslayjema@gmail.com>
2023-04-03 00:55:10 +0300

    style:change font size on tablet

--- client/src/components/PopularAdventure.js
+++ client/src/components/PopularAdventure.js
@@ -14,19 +14,19 @@ const PopularAdventure = () => {
         </div>
         <h1 className="font-bold text-center my-2 text-lg lg:text-3xl">Popular Expeditures</h1>
         <div className="grid grid-cols-3 gap-x-6 mx-4 sm:mx-20 ">
-            <div className=' rounded-lg h-32 sm:h-60 md:h-50' > 
+            <div className=' rounded-lg h-32 sm:h-60 md:h-50 popularcont' > 
                 <Image src="/safari-1.jpg" alt='nnn' height={500} width={500} className="w-full  h-full object-cover  rounded-lg"/> 
-                <h3 className="sm:text-sm lg:text-2xl font-bold mb-3">Serengeti</h3>
+                <h3 className="sm:text-sm lg:text-2xl font-bold mb-3 des">Serengeti</h3>
             </div>
 
-            <div className='rounded-lg h-32 sm:h-60 md:h-50'>
+            <div className='rounded-lg h-32 sm:h-60 md:h-50 popularcont'>
                 <Image src="/kilimanjaro.jpg" alt='nnn'  priority={true} height={500} width={500} className="w-full h-full object-cover  rounded-lg" />  
-                <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3">Kilimanjaro</h3>
+                <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3 des">Kilimanjaro</h3>
             </div>
 
-            <div className=' rounded-lg h-32 sm:h-60 md:h-50'>
+            <div className=' rounded-lg h-32 sm:h-60 md:h-50 popularcont'>
                 <Image src="/tarangire.jpg" alt='nnn' height={500} width={500} className="w-full  h-full object-cover  rounded-lg"/> 
-                <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3">Tarangire</h3>
+                <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3 des">Tarangire</h3>
             </div>
         </div>
       </section>
