ad3ebd1a17ccff7bade49276f66ee626c58b1333
queenslay jema <queenslayjema@gmail.com>
2023-04-05 02:58:15 +0300

    Feat:create seed file for mountain

---
+++ db/mountains/mountains.rb
@@ -0,0 +1,9 @@
+meru = Mountain.create(
+    mountain_name: "Mount Meru", 
+    description: "The fifth highest mountain in Tanzania."
+)
+kilimanjaro = Mountain.create(
+    mountain_name: "Mount Kilimanjaro", 
+    description: "The highest mountain in Tanzania and Africa."
+)
+ 
