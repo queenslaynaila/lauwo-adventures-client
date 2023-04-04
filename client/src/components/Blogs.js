09e8a934ef083d31da4f5904b5964550ff5e84e5
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-04-04 09:21:31 +0300

    tyled the recent blog components

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -1,146 +1,56 @@
 import Image from 'next/image';
+import { blogs } from './Data.js/data';
 
 function Blogs() {
   return (
-    <div className='m-4'>
+    <div className="m-4">
       <div>
-        <h1 className="font-bold text-center  text-4xl mt-5"> RECENT BLOGS</h1>
+        <h1 className="font-bold text-center text-4xl mt-5">RECENT BLOGS</h1>
       </div>
-
-      <div className="grid grid-cols-1 gap-5 md:grid-cols-3 md:gap-8 mt-4">
-        <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <Image
-            src={`/b-1.jpg`}
-            alt="Picture of the author"
-            width={900}
-            height={900}
-          />
-          <div className="p-5">
-            <h2 className=" px-4 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              We Travel Not To Escape Life
-              But  For Life Not To Escape.
-            </h2>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <div className="flex justify-between">
-              <a
-                href="#"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  aria-hidden="true"
-                  className="w-4 h-4 ml-2 -mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                 
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </a>
-              <div className="">
-                <p>20th February 2023</p>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {blogs.map((val, index) => (
+          <div className="p-4" key={val.title}>
+            <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-[460px] max-w-[800px]">
+              <Image src={val.image} alt="/" width={800} height={600} className="h-48 w-full object-cover rounded-t-lg" />
+              <div className="p-5 h-48">
+                <h2 className="px-4 mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                  {val.title}
+                </h2>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 h-24 overflow-hidden">
+                  {val.description}
+                </p>
+                <div className="flex justify-between mt-3">
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-1 py-2  text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Read more
+                    <svg
+                      aria-hidden="true"
+                      className="w-3 h-3 ml-2 -mr-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                  <div className="">
+                    <p>{val.date}</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <Image
-            src={`/b-2.jpg`}
-            alt="Picture of the author"
-            width={900}
-            height={900}
-          />
-          <div className="p-5">
-            <h2 className=" px-4 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Road Map To Mountain Climbing. 
-            </h2>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <div className="flex justify-between">
-              <a
-                href="#"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  aria-hidden="true"
-                  className="w-4 h-4 ml-2 -mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </a>
-              <div className="">
-                <p>3rd January 2023</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        <div className="bg-white border border-gray-200 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700 object-contain">
-          <Image
-            src={`/b-6.jpg`}
-            alt="Picture of the compass"
-            width={900}
-            height={900}
-          />
-          <div className="p-5">
-            <h2 className=" px-4  mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              I Haven't Been  EveryWhere 
-             
-              But It's On My List.
-            </h2>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <div className="flex justify-between ">
-              <a
-                href="#"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  aria-hidden="true"
-                  className="w-4 h-4 ml-2 -mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                 
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </a>
-              <div className="">
-                <p>12th December 2022</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
-
-      <div className= 'mt-8  grid  place-items-center'>
-      <button class="bg-yellow-500 font-bold py-2 px-4 mt-3 rounded items-center">View All</button>
+      <div className="mt-8 grid place-items-center">
+        <button className="bg-yellow-500 font-bold py-2 px-4 mt-3 rounded items-center">
+          View All
+        </button>
       </div>
     </div>
   );
---
+++ client/src/components/Data.js/data.js
@@ -0,0 +1,29 @@
+import b1 from '../assets/b-1.jpg';
+import b2 from '../assets/b-2.jpg';
+import b3 from '../assets/b-6.jpg'
+
+
+ 
+export const blogs =  [{
+    title: " Here are the biggest enterprise technology acquisitions of 2021 sofar.",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five countries.",
+    image :b1,
+    date: "20th February 2023",
+  },
+  {
+    title: "Road Map To Mountain Climbing.",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    image :b2,
+    date: "3rd January 2023",
+  },
+  {
+    
+    title: " I Haven't Been  EveryWhere But It's On My List.",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+    image :b3,
+    date: "12th December 2022",
+  }
+]
+
+    
+
