b51ce5fd1269246df757ab9e819d9a613cae164f
queenslay jema <queenslayjema@gmail.com>
2023-04-04 19:37:00 +0300

    Edit:Add description and image fields to routes table

--- db/migrate/20230404033556_create_routes.rb
+++ db/migrate/20230404033556_create_routes.rb
@@ -3,7 +3,8 @@ class CreateRoutes < ActiveRecord::Migration[7.0]
     create_table :routes do |t|
       t.string :route_name
       t.integer :mountain_id
-
+      t.text :description
+      t.string :image_URL
       t.timestamps
     end
   end
