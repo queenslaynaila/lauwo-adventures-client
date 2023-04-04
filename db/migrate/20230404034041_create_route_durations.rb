d23ac0e015c43603c5f53fbf6c472a13958c6cc3
queenslay jema <queenslayjema@gmail.com>
2023-04-04 19:38:34 +0300

    edit:Add title,standard & premium price

--- db/migrate/20230404034041_create_route_durations.rb
+++ db/migrate/20230404034041_create_route_durations.rb
@@ -2,10 +2,14 @@ class CreateRouteDurations < ActiveRecord::Migration[7.0]
   def change
     create_table :route_durations do |t|
       t.integer :route_id
+      t.string :title
       t.integer :duration_days
-      t.integer :price
-      t.text :inclusive
-      t.text :exclusive
+      t.integer :standard_price
+      t.integer :premium_price
+      t.text    :standard_inclusive
+      t.text    :standard_exclusive
+      t.text    :premium_inclusive
+      t.text    :premium_exclusive
 
       t.timestamps
     end
