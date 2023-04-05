96f334337ebdcbaad464f63702761cf890ea7e20
queenslay jema <queenslayjema@gmail.com>
2023-04-05 23:51:37 +0300

    Feat:Update mountain route has many through rlshp

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,3 +1,5 @@
 class Mountain < ApplicationRecord
     has_many :routes
+    has_many :route_durations, through: :routes
+    has_many :itineries, through: :route_durations
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,5 @@
 class Route < ApplicationRecord
     belongs_to :mountain
     has_many :route_durations
+    has_many :itineries, through: :route_durations
 end
