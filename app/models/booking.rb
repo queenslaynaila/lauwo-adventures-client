60fcdbe07d0229b36b4393706b83b1a91b790c57
queenslay jema <queenslayjema@gmail.com>
2023-04-06 02:08:12 +0300

    Feat:do validations for the booking form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,8 @@
 class Booking < ApplicationRecord
     belongs_to :route_duration
+    validates :first_name, :last_name, :email, :phone_number, :country, :travel_period, :no_of_travellers, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :phone_number, format: { with: /\A\+?\d{1,3}[- ]?\d{9,11}\z/, message: "should be a valid phone number with country code" }
+    validates :no_of_travellers, numericality: { only_integer: true, greater_than: 0 }
+    validates :additional_info, length: { maximum: 500 }
 end
