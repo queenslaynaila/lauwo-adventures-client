b43b1143937928506c33b30486b7503ec1515f5f
queenslay jema <queenslayjema@gmail.com>
2023-04-05 04:03:07 +0300

    edit:edit mountain data to match ui

--- db/mountains/mountains.rb
+++ db/mountains/mountains.rb
@@ -1,9 +1,9 @@
 meru = Mountain.create(
     mountain_name: "Mount Meru", 
-    description: "The fifth highest mountain in Tanzania."
+    description: "Mount Meru is a dormant stratovolcano located 70 kilometres west of Mount Kilimanjaro in southeast Arusha Region, Tanzania."
 )
 kilimanjaro = Mountain.create(
     mountain_name: "Mount Kilimanjaro", 
-    description: "The highest mountain in Tanzania and Africa."
+    description: "Mount Kilimanjaro is a dormant volcano in Tanzania. It has three volcanic cones..."
 )
  
