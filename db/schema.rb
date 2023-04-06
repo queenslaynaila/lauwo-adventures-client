9dc93e592c148987e5ceadffb789fc9f5c902cd2
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-04-06 12:36:50 +0300

    create adventures

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_084130) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_093252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "adventures", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.string "first_name"
@@ -70,12 +76,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_084130) do
     t.integer "mountain_id"
     t.text "description"
     t.string "image_URL"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "services", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
