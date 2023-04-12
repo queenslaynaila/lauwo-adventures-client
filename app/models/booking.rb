2f954833a9d02be401bde7bf033c068b9cfe1bb1
queenslay jema <queenslayjema@gmail.com>
2023-04-12 09:38:54 +0300

    Feat:add error messsages to validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,8 +5,8 @@ class Booking < ApplicationRecord
     validates :route_duration_id, presence: true, unless: :safari_id?
 
     validates :first_name, :last_name, :email, :phone_number, :country, :travel_period, :no_of_travellers, presence: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP,message: "must be a valid email address" }
     validates :phone_number, format: { with: /\A\+?\d{1,3}[- ]?\d{9,11}\z/, message: "should be a valid phone number with country code" }
-    validates :no_of_travellers, numericality: { only_integer: true, greater_than: 0 }
-    validates :additional_info, length: { maximum: 500 }
+    validates :no_of_travellers, numericality: { only_integer: true, greater_than: 0, message: "must be a positive number" }
+    validates :additional_info, length: { maximum: 500,message: "cannot exceed 500 words" }
 end
