585bcfc0469298834146fc69f4af15e53042295b
queenslay jema <queenslayjema@gmail.com>
2023-04-17 00:23:17 +0300

    Feat:Move form arrays to a data file

---
+++ client/src/data/planningData.js
@@ -0,0 +1,8 @@
+export const adventures = ["Mountain Trekking","Safaris","Day Trips","Cultural Tours"]
+export const months = [
+   "Dont Know or Not sure", "January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"
+  ];
+export const Budget =[
+    "Dont Know or Not sure","Budget","Mid range","Luxury","Top Luxury"
+]
