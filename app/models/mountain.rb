f0035084cf511a70a0786563f359d250f715e25a
queenslay jema <queenslayjema@gmail.com>
2023-04-17 06:05:46 +0300

    Feat:generate uuid before creating mountain

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,7 +1,14 @@
+require 'securerandom'
 class Mountain < ApplicationRecord
+    before_create :generate_uuid
     belongs_to :adventure
     has_many :routes
     has_many :route_durations, through: :routes
     has_many :itineries, through: :route_durations
     has_many :bookings, through: :route_durations
+
+   private
+   def generate_uuid
+    self.uuid = SecureRandom.uuid
+   end
 end
