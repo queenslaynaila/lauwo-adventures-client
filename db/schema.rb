4b2c807d82d6f51550b11f8e432f68f9837b866f
queenslay jema <queenslayjema@gmail.com>
2023-04-17 06:06:00 +0300

    Feat:schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_020745) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_030100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -121,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_020745) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.string "uuid"
     t.index ["adventure_id"], name: "index_mountains_on_adventure_id"
   end
 
