eeefe774b579cebd794c9c54a1243cec14b72f86
queenslay jema <queenslayjema@gmail.com>
2023-04-19 08:29:57 +0300

    Feat:route serializer

--- app/serializers/route_serializer.rb
+++ app/serializers/route_serializer.rb
@@ -1,5 +1,6 @@
 class RouteSerializer < ActiveModel::Serializer
   attributes :id, :route_name, :description, :image_URL, :durations
+  has_many :route_durations
 
   def durations
     object.route_durations.pluck(:duration_days)
