9df56cf5631583d48f0b32e927d41077e494d862
queenslay jema <queenslayjema@gmail.com>
2023-04-19 11:29:42 +0300

    Feat:make itnery dynamic

--- client/src/components/MountainItinery.js
+++ client/src/components/MountainItinery.js
@@ -1,46 +1,26 @@
  
 
-export default function MountainItinery() {
+export default function MountainItinery({itinery,index}) {
+  console.log(index)
   return (
-    <div class="relative">
-    <div class="vl absolute  border-l-2 border-black ml-[-3px] left-1/2 " style={{height:"95%",top:"5%"}}>
-    </div>
-   <div class="flex justify-center bg-sand">
-      <div class="w-1/2 m-4 relative">
-        <div class="absolute top-0 left-0   flex items-center justify-center w-16 h-16   text-white text-6xl font-bold">01
+    
+    <div>
+    
+    <div class={`flex justify-center ${index % 2 === 0 ? "bg-sand" : "bg-light"}`}>
+     
+      <div className={`w-1/2 m-4 relative ${index % 2 !== 0 ? "order-last" : ""}`}>
+        <div class="absolute top-0 left-0   w-16 h-16 text-white text-6xl font-bold">{itinery.day_no}</div>
+        <div class="h-full flex items-center justify-center text-black pt-14" >
+          <h1 className='text-2xl font-bold'>{itinery.title.toUpperCase()}</h1>
         </div>
-        <div class="h-full flex items-center justify-center text-black rounded-full">
-            <h1 className='text-2xl font-bold'>Arrival and Pickup</h1>
-        </div>
       </div>
-      <div class="w-1/2 m-4">
-        <p >We trekn across picturesque moorland meadows, making our way to the Shira 1 Camp where we'll enjoy a delicious dinner and spend the night. Along the way, we'll be treated to breathtaking views of the valleys, cathedral like structures that make up the Shira caldera and plane land of Western Kilimanjaro. As the sun sets, temperatures will drop significantly, making for a much chillier night than the previous one.</p>
-      </div>
-   </div>
-    <div class="flex justify-center" style={{backgroundColor:"#FFF4D2"}}>
-      <div class="w-1/2 m-4  ">
-        <p>We trek across picturesque moorland meadows, making our way to the Shira 1 Camp where we'll enjoy a delicious dinner and spend the night. Along the way, we'll be treated to breathtaking views of the valleys, cathedral like structures that make up the Shira caldera and plane land of Western Kilimanjaro. As the sun sets, temperatures will drop significantly, making for a much chillier night than the previous one.</p>
-      </div>
-      <div class="w-1/2 m-4 relative">
-        <div class="absolute top-0 left-0   flex items-center justify-center w-16 h-16   text-white text-6xl font-bold  ">01</div>
-          <div class="h-full flex items-center justify-center text-black rounded-full">
-            <h1 className='text-2xl font-bold'>Arrival and Pickup</h1>
-          </div>
+      <div className={`w-1/2 m-4 ${index % 2 !== 0 ? "" : "order-last"}`}>
+      
+       
+        <p>{itinery.description}</p>
       </div>
     </div>
-    <div class="flex justify-center bg-sand">
-      <div class="w-1/2 m-4 relative">
-        <div class="absolute top-0 left-0   flex items-center justify-center w-16 h-16   text-white text-6xl font-bold">01
-        </div>
-        <div class="h-full flex items-center justify-center text-black rounded-full">
-            <h1 className='text-2xl font-bold'>Arrival and Pickup</h1>
-        </div>
-      </div>
-      <div class="w-1/2 m-4">
-        <p >We trekn across picturesque moorland meadows, making our way to the Shira 1 Camp where we'll enjoy a delicious dinner and spend the night. Along the way, we'll be treated to breathtaking views of the valleys, cathedral like structures that make up the Shira caldera and plane land of Western Kilimanjaro. As the sun sets, temperatures will drop significantly, making for a much chillier night than the previous one.</p>
-      </div>
-    </div>
-  
   </div>
+  
   )
 }
