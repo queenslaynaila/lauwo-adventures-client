1e34fc75705e2541d19eafac2898fe66fc31a503
queenslay jema <queenslayjema@gmail.com>
2023-04-20 02:24:19 +0300

    Style:header text

--- client/src/pages/blogs/index.js
+++ client/src/pages/blogs/index.js
@@ -5,8 +5,8 @@ export default function index({blogs}) {
       <div className='font-poly'>
         <header className="w-full h-64 lg:h-96 bg-[url('/safari-1.jpg')] bg-cover bg-center flex justify-center items-center font-poly">
           <div role="main" class="flex flex-col items-center justify-center">
-            <h1 class="text-2xl font-semibold leading-9 text-center lg:w-1/2 md:w-10/12 w-11/12 ml-4"> Explore Our Latest Travel Stories</h1>
-            <p class="text-base leading-normal text-center mt-4 lg:w-1/2 md:w-10/12 w-11/12">Discover the hidden gems of Tanzania with our expert guides. From the majestic peaks of Kilimanjaro to thrilling safaris and cultural experiences, our blog has it all</p>
+            <h1 class="text-2xl  font-semibold leading-9 text-center lg:w-1/2 md:w-10/12 w-11/12 ml-4"> Explore Our Latest Travel Stories</h1>
+            <p class="text-base leading-normal text-center   mt-4 lg:w-1/2 md:w-10/12 w-11/12">Discover the hidden gems of Tanzania with our expert guides. From the majestic peaks of Kilimanjaro to thrilling safaris and cultural experiences, our blog has it all</p>
           </div>
         </header>
         <main class="lg:relative  lg:-top-40    py-12 md:px-20 sm:px-14 px-6">
