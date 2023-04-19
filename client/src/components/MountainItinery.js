6f06752f4769664e5b97898153aaa1cda6cce163
queenslay jema <queenslayjema@gmail.com>
2023-04-19 08:58:14 +0300

    Feat:Create a single itinery component

---
+++ client/src/components/MountainItinery.js
@@ -0,0 +1,46 @@
+ 
+
+export default function MountainItinery() {
+  return (
+    <div class="relative">
+    <div class="vl absolute  border-l-2 border-black ml-[-3px] left-1/2 " style={{height:"95%",top:"5%"}}>
+    </div>
+   <div class="flex justify-center bg-sand">
+      <div class="w-1/2 m-4 relative">
+        <div class="absolute top-0 left-0   flex items-center justify-center w-16 h-16   text-white text-6xl font-bold">01
+        </div>
+        <div class="h-full flex items-center justify-center text-black rounded-full">
+            <h1 className='text-2xl font-bold'>Arrival and Pickup</h1>
+        </div>
+      </div>
+      <div class="w-1/2 m-4">
+        <p >We trekn across picturesque moorland meadows, making our way to the Shira 1 Camp where we'll enjoy a delicious dinner and spend the night. Along the way, we'll be treated to breathtaking views of the valleys, cathedral like structures that make up the Shira caldera and plane land of Western Kilimanjaro. As the sun sets, temperatures will drop significantly, making for a much chillier night than the previous one.</p>
+      </div>
+   </div>
+    <div class="flex justify-center" style={{backgroundColor:"#FFF4D2"}}>
+      <div class="w-1/2 m-4  ">
+        <p>We trek across picturesque moorland meadows, making our way to the Shira 1 Camp where we'll enjoy a delicious dinner and spend the night. Along the way, we'll be treated to breathtaking views of the valleys, cathedral like structures that make up the Shira caldera and plane land of Western Kilimanjaro. As the sun sets, temperatures will drop significantly, making for a much chillier night than the previous one.</p>
+      </div>
+      <div class="w-1/2 m-4 relative">
+        <div class="absolute top-0 left-0   flex items-center justify-center w-16 h-16   text-white text-6xl font-bold  ">01</div>
+          <div class="h-full flex items-center justify-center text-black rounded-full">
+            <h1 className='text-2xl font-bold'>Arrival and Pickup</h1>
+          </div>
+      </div>
+    </div>
+    <div class="flex justify-center bg-sand">
+      <div class="w-1/2 m-4 relative">
+        <div class="absolute top-0 left-0   flex items-center justify-center w-16 h-16   text-white text-6xl font-bold">01
+        </div>
+        <div class="h-full flex items-center justify-center text-black rounded-full">
+            <h1 className='text-2xl font-bold'>Arrival and Pickup</h1>
+        </div>
+      </div>
+      <div class="w-1/2 m-4">
+        <p >We trekn across picturesque moorland meadows, making our way to the Shira 1 Camp where we'll enjoy a delicious dinner and spend the night. Along the way, we'll be treated to breathtaking views of the valleys, cathedral like structures that make up the Shira caldera and plane land of Western Kilimanjaro. As the sun sets, temperatures will drop significantly, making for a much chillier night than the previous one.</p>
+      </div>
+    </div>
+  
+  </div>
+  )
+}
