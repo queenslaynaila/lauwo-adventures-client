6859cdf89160168c8e5a7a1b021b2cbb56f9ce43
queenslay jema <queenslayjema@gmail.com>
2023-04-19 23:28:05 +0300

    Feat:updatte saticprops and staticpaths for blogs

--- client/src/pages/blogs/[blog].js
+++ client/src/pages/blogs/[blog].js
@@ -1,9 +1,30 @@
- 
+import { generateSlug } from "@/utils/generateSlug";
 
-export default function blog() {
+export default function blog({blog}) {
+  console.log(blog)
   return (
     <div>
       single blog
     </div>
   )
+}
+
+export async function getStaticPaths() {
+  const res = await fetch('http://localhost:3000/blogs');
+  const blogs = await res.json();
+  const paths = blogs.map((blog) => ({
+    params: { blog: generateSlug(blog.title), id: blog.id },
+  }));
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch('http://localhost:3000/blogs');
+  const blogs = await res.json();
+  const blog = blogs.find(
+    (blog) => generateSlug(blog.title) === params.blog
+  );
+  return {
+    props: { blog },
+  };
 }
