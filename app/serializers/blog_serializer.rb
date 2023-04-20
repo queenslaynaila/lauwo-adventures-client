ef7e712e8505cd345960928f8829502c0c799e27
queenslay jema <queenslayjema@gmail.com>
2023-04-20 17:22:07 +0300

    Feat:modify rails created at updated time format

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,3 +1,10 @@
 class BlogSerializer < ActiveModel::Serializer
   attributes  :image_url, :id, :title ,:content ,:author ,:created_at ,:updated_at
+  def created_at
+    object.updated_at.strftime("%A %e %B %Y %l:%M %p")
+  end
+
+  def updated_at
+    object.updated_at.strftime("%A %e %B %Y %l:%M %p")
+  end
 end
