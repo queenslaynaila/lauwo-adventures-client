19350243cb1623060d9e87afa1dede037f5e08e1
queenslay jema <queenslayjema@gmail.com>
2023-04-20 14:59:38 +0300

    Fix:update validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,7 @@ class Booking < ApplicationRecord
     belongs_to :bookable, polymorphic: true
     validates :first_name, :last_name, :email, :phone_number, :country, :travel_period, :no_of_travellers, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP,message: "must be a valid email address" }
+    validates :no_of_travellers, numericality: { greater_than_or_equal_to: 1, message: "must be at least 1" }
+  validates :no_of_adults, numericality: { greater_than_or_equal_to: 1, message: "must be at least 1" }
     validates :additional_info, length: { maximum: 500,message: "cannot exceed 500 words" }
 end
