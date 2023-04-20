ed94e0916bc1f90d05e451aff85e842d85c93822
queenslay jema <queenslayjema@gmail.com>
2023-04-20 20:18:02 +0300

    Feat:add padding

--- client/src/pages/blogs/index.js
+++ client/src/pages/blogs/index.js
@@ -9,7 +9,7 @@ export default function index({blogs}) {
             <p class="text-base leading-normal text-center   mt-4 lg:w-1/2 md:w-10/12 w-11/12">Discover the hidden gems of Tanzania with our expert guides. From the majestic peaks of Kilimanjaro to thrilling safaris and cultural experiences, our blog has it all</p>
           </div>
         </header>
-        <main class="lg:relative  lg:-top-40    py-12 md:px-20 sm:px-14 px-6">
+        <main class="lg:relative  lg:-top-40  md:px-20 sm:px-14 px-6">
           <div class="mt-2 md:flex space-x-6">
                 {blogs.map((blog) => (
                   <BlogCard blog={blog} />
