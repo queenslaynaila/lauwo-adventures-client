d3e9d0fbfb30fe753418255dda0df40d3bd47fff
queenslay jema <queenslayjema@gmail.com>
2023-04-20 21:08:18 +0300

    Feat:add hostnames to config

--- client/next.config.js
+++ client/next.config.js
@@ -9,11 +9,12 @@ const nextConfig = {
       'plus.unsplash.com',
       'explorerspassage.com',
       'www.shadowsofafrica.com',
-      'www.discoverafricablog.com',
-      'www.willflyforfood.net',
-'media-cdn.tripadvisor.com'
+'media-cdn.tripadvisor.com',
+'www.discoverafricablog.com',
+'www.willflyforfood.net',
     ],
   },
 };
 
+module.exports = nextConfig;
 module.exports = nextConfig;
