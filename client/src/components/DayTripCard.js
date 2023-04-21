ce9fd203d9e0b485ae27a9f6a4b437aa75c3cb07
queenslay jema <queenslayjema@gmail.com>
2023-04-21 11:44:27 +0300

    Feat:day trips container view

---
+++ client/src/components/DayTripCard.js
@@ -0,0 +1,20 @@
+ 
+export default function DayTripCard({day}) {
+  return (
+    <div>
+       <div class="relative card overflow-hidden rounded-lg shadow-md hover:shadow-lg transform transition-transform hover:-translate-y-1 cursor-pointer h-80">
+    <img src={day.image_url} class="card__image w-full h-full object-cover"/> 
+    <div class="absolute inset-0 flex items-center justify-center text-left">
+      <div class="text-white p-4">
+        <h3 class="text-2xl font-bold mt-4">{day.name}</h3>
+        <div class="absolute bottom-0 left-0 px-2 bg-yellow-500">
+          <div class="text-lg font-medium">{day.price}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+  
