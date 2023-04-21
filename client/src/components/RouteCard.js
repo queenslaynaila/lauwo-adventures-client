2375ceb5e9cbe98c611ecaa07836566c9888cf84
queenslay jema <queenslayjema@gmail.com>
2023-04-21 17:32:35 +0300

    Feat:update booking parametres

--- client/src/components/RouteCard.js
+++ client/src/components/RouteCard.js
@@ -1,30 +1,65 @@
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import BookingForm from '@/components/BookingForm';
+import Image from 'next/image';
+
+export default function RouteCard({ route, duration,routeDuration }) {
+  
+  const bookableType ='RouteDuration' 
  
-import Image from 'next/image'
-export default function RouteCard({route,duration,bookableType}) {
-    
+  const adventure = {name:`${route.route_name} ${duration} day climb` ,id:routeDuration}
+  const contentStyle = {  width: '85%', maxHeight: '85%', overflow: 'auto', margin: 'auto'};
+
   return (
-     
-      <div className='bg-sand font-poly'>
+    <div className="bg-sand font-poly">
       <div className="flex items-center justify-center">
-           <Image src="/peakfinder.png" alt='nnn' height={50} width={50} className='mr-2' />
-           <h3 className="lg:text-2xl xl:text-3xl 3xl:text-4xl text-xl capitalize text-center text-black py-32 ">{route.route_name} | {duration} Days </h3>
+        <Image
+          src="/peakfinder.png"
+          alt="nnn"
+          height={50}
+          width={50}
+          className="mr-2"
+        />
+        <h3 className="lg:text-2xl xl:text-3xl 3xl:text-4xl text-xl capitalize text-center text-black py-32 ">
+          {route.route_name} | {duration} Days{" "}
+        </h3>
       </div>
       <div className="flex flex-col items-center px-8 -mt-24">
         <div className="flex relative flex-col md:flex-row md:items-center mb-28">
           <div className="md:w-1/2">
-              <img src={route.image_URL} alt="Route" className="rounded-lg mb-4 h-96 object-cover object-center" />
+            <img
+              src={route.image_URL}
+              alt="Route"
+              className="rounded-lg mb-4 h-96 object-cover object-center"
+            />
           </div>
           <div className="md:w-1/2 md:ml-8">
-                <p className="mb-4 text-lg leading-6">{route.description}</p>
-                <div className="text-center h-24">
-                  <button className="mt-auto bg-yellow-400 text-black py-2 px-4 md:w-1/5">
+            <p className="mb-4 text-lg leading-6">{route.description}</p>
+            <div className="text-center h-24">
+              <Popup
+                trigger={
+                  <button className="bg-black text-white px-8 rounded-md py-4 ml-auto mb-16 hover:border-black hover:border hover:text-black hover:bg-transparent">
                     Book Slot
                   </button>
-                </div>
+                }
+                modal
+                nested
+                closeOnDocumentClick
+                contentStyle={contentStyle}
+              >
+                {(close) => (
+                  <div className="modal">
+                    <button className="close" onClick={close}>
+                      &times;
+                    </button>
+                    <BookingForm adventure={adventure} bookableType={bookableType}/>
+                  </div>
+                )}
+              </Popup>
+            </div>
           </div>
         </div>
       </div>
     </div>
-   
-  )
+  );
 }
