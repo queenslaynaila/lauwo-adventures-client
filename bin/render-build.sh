b3e5038315464fc454da0c7de29b180058ee264f
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-04-22 21:06:07 +0300

    remove seed command from deploying commands

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,4 +10,3 @@ cp -a client/build/. public/
 # Build commands for back end
 bundle install
 bundle exec rake db:migrate
-bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
