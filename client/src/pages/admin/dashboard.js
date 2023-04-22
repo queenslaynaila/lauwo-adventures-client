7c6332eab385c4fee65cb6e83602688b06ed028f
queenslay jema <queenslayjema@gmail.com>
2023-04-23 00:45:39 +0300

    Feat:include dashboard page for redirects

--- client/src/pages/admin/dashboard.js
+++ client/src/pages/admin/dashboard.js
@@ -13,10 +13,9 @@ const Dashboard = () => {
     <>
       <div
         className="bg-cover bg-center bg-fixed h-screen"
-        style={{ backgroundImage: "url('/g-2.jpg')" }}
-      >
+        style={{ backgroundImage: "url('/g-2.jpg')" }} >
        
-       
+       <h1>THis will be the dashboard</h1>
       
       </div> 
        
