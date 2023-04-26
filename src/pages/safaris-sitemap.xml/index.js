645d3df230b537a8348dc3cd3a4411221336dd21
queenslay jema <queenslayjema@gmail.com>
2023-04-26 23:56:27 +0300

    Feat:add a safaris sitemap

---
+++ src/pages/safaris-sitemap.xml/index.js
@@ -0,0 +1,19 @@
+import {getServerSideSitemapLegacy } from "next-sitemap";
+import { generateSlug } from '@/utils/generateSlug'
+export const getServerSideProps = async (ctx) => {
+  
+  const safaris = await fetch("http://localhost:3000/safaris").then(res => res.json());
+
+   
+  const safariSitemaps = safaris.map((item) => ({
+    loc: `${process.env.NEXT_PUBLIC_DOMAIN_URL}safaris/${generateSlug(item.name)}`,
+    lastmod: new Date().toISOString(),
+  }));
+
+
+  const fields = [...safariSitemaps ];
+   
+  return  getServerSideSitemapLegacy(ctx, fields);
+};
+
+export default function Site() {}
