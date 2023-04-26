ba604e396d165922477001c38231a0572f038536
queenslay jema <queenslayjema@gmail.com>
2023-04-26 23:55:51 +0300

    Feat:configure next-sitemap package

---
+++ next-sitemap.config.js
@@ -0,0 +1,22 @@
+const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
+module.exports = {
+  siteUrl,
+  exclude: ["/404,/admin"],
+  generateRobotsTxt: true,
+  sourceDir: './build', 
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: ["/404,/admin"],
+      },
+    ],
+    additionalSitemaps: [
+      `${siteUrl}sitemap.xml`,
+      `${siteUrl}mountains-sitemap.xml`,
+      `${siteUrl}mountain-routes-sitemap.xml`,
+      `${siteUrl}safaris-sitemap.xml`,
+      `${siteUrl}day-trips-sitemap.xml`,
+    ],
+  },
+};
