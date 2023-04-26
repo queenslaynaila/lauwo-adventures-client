b3c4cf376fd12d9976d369b8cac0886ff6e1e711
queenslay jema <queenslayjema@gmail.com>
2023-04-26 23:56:47 +0300

    Feat:add mountains sitemap

---
+++ src/pages/mountains-sitemap.xml/index.js
@@ -0,0 +1,18 @@
+import { getServerSideSitemapLegacy } from "next-sitemap";
+import { generateSlug } from '@/utils/generateSlug'
+
+export const getServerSideProps = async (ctx) => {
+  const mountains = await fetch("http://localhost:3000/mountains").then(res => res.json());
+   
+  const mountainSitemaps = mountains.map((item) => ({
+    loc: `${process.env.NEXT_PUBLIC_DOMAIN_URL}mountain-trekking/${generateSlug(item.mountain_name)}`,
+    lastmod: new Date().toISOString(),
+  }));
+
+   
+  const fields = [...mountainSitemaps];
+
+  return getServerSideSitemapLegacy(ctx, fields);
+};
+
+export default function Site() {}
