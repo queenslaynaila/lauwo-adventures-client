60a9f5e4e1af40a9d2d31cf9fab75a8df554ac52
queenslay jema <queenslayjema@gmail.com>
2023-05-02 17:20:57 +0300

    Feat:add user section

--- src/components/dashboard/Users.js
+++ src/components/dashboard/Users.js
@@ -91,80 +91,72 @@ const Users = () => {
         </div>
       </div>
 
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg w-full">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div class="relative shadow-md sm:rounded-lg    "  >
+        <table class=" w-full text-sm text-left text-gray-500 ">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
             <tr>
               <th scope="col" class="px-6 py-3">
-                Product name
+                EMAIL
               </th>
               <th scope="col" class="px-6 py-3">
-                <div class="flex items-center">Color</div>
+                <div class="flex items-center">ROLE</div>
               </th>
+              
               <th scope="col" class="px-6 py-3">
-                <div class="flex items-center">Category</div>
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <div class="flex items-center">Price</div>
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <span class="sr-only">Edit</span>
+              <div class="flex items-center">ACTION</div>
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="bg-white border-b">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
               >
                 Apple MacBook Pro 17"
               </th>
               <td class="px-6 py-4">Silver</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
-              <td class="px-6 py-4 text-right">
+               
+              <td class="px-6 py-4 ">
                 <a
                   href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  class="font-medium text-blue-600  hover:underline"
                 >
                   Edit
                 </a>
               </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="bg-white border-b   ">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap  "
               >
                 Microsoft Surface Pro
               </th>
               <td class="px-6 py-4">White</td>
-              <td class="px-6 py-4">Laptop PC</td>
-              <td class="px-6 py-4">$1999</td>
-              <td class="px-6 py-4 text-right">
+               
+              <td class="px-6 py-4  ">
                 <a
                   href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  class="font-medium text-blue-600 hover:underline"
                 >
                   Edit
                 </a>
               </td>
             </tr>
-            <tr class="bg-white dark:bg-gray-800">
+            <tr class="bg-white  ">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
               >
                 Magic Mouse 2
               </th>
               <td class="px-6 py-4">Black</td>
-              <td class="px-6 py-4">Accessories</td>
-              <td class="px-6 py-4">$99</td>
-              <td class="px-6 py-4 text-right">
+              
+              <td class="px-6 py-4  ">
                 <a
                   href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  class="font-medium text-blue-600  hover:underline"
                 >
                   Edit
                 </a>
