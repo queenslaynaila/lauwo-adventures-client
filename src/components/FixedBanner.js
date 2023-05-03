a3ce6dd54e8657c6e14c9cd5a462aa2aadeca4eb
queenslay jema <queenslayjema@gmail.com>
2023-05-03 14:26:01 +0300

    Feat:content banner complete

--- src/components/FixedBanner.js
+++ src/components/FixedBanner.js
@@ -20,18 +20,24 @@ const FixedBanner = () => {
 
   return (
     <>
-  {banners.length > 0 && banners.map((banner) => (
-    <div key={banner.id} className="absolute lg:top-[18%]  top-[10%] right-0 left-0  flex flex-col   w-full">
-      <div className="bg-yellow-400 text-white p-4 flex justify-between items-center">
-        <p className="text-lg">{banner.message}</p>
-        <button onClick={handleCloseBanner} className="text-white text-2xl">
-          <IoClose />
-        </button>
-      </div>
-    </div>
-  ))}
-</>
-
+      {banners.length > 0 &&
+        banners.map((banner) => (
+          <div
+            key={banner.id}
+            className="absolute lg:top-[18%]  top-[10%] right-0 left-0  flex flex-col   w-full"
+          >
+            <div className="bg-yellow-400 text-white p-4 flex justify-between items-center">
+              <p className="text-lg">{banner.message}</p>
+              <button
+                onClick={handleCloseBanner}
+                className="text-white text-2xl"
+              >
+                <IoClose />
+              </button>
+            </div>
+          </div>
+        ))}
+    </>
   );
 };
 
