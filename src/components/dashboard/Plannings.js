4f13ac4b4bdabca319e02ac7d361194ce8382edc
queenslay jema <queenslayjema@gmail.com>
2023-05-04 16:36:40 +0300

    Feat:wrap table in an verflow container

--- src/components/dashboard/Plannings.js
+++ src/components/dashboard/Plannings.js
@@ -1,146 +1,232 @@
-import React, { useState } from 'react';
+import { useState, useMemo, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {
+  useTable,
+  useSortBy,
+  useFilters,
+  useGlobalFilter,
+  usePagination,
+} from 'react-table';
 
-const usersData = [
-  {
-    id: 1,
-    username: 'john_doe',
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-    role: 'admin',
-  },
-  {
-    id: 2,
-    username: 'jane_doe',
-    name: 'Jane Doe',
-    email: 'jane.doe@example.com',
-    role: 'user',
-  },
-  {
-    id: 3,
-    username: 'bob_smith',
-    name: 'Bob Smith',
-    email: 'bob.smith@example.com',
-    role: 'user',
-  },
-];
+export default function Plannings() {
+  const [data, setData] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-const Plannings = () => {
-  const [search, setSearch] = useState('');
-  const [selectedUsers, setSelectedUsers] = useState([]);
-  const [users, setUsers] = useState(usersData);
+  useEffect(() => {
+    fetch('http://localhost:3000/planningforms')
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
 
-  const handleSearch = (event) => {
-    setSearch(event.target.value);
-  };
-
-  const handleCheckboxChange = (event, user) => {
-    if (event.target.checked) {
-      setSelectedUsers((prevSelectedUsers) => [...prevSelectedUsers, user]);
-    } else {
-      setSelectedUsers((prevSelectedUsers) =>
-        prevSelectedUsers.filter((selectedUser) => selectedUser.id !== user.id)
-      );
-    }
-  };
-
-  const handleBulkDelete = () => {
-    setUsers((prevUsers) =>
-      prevUsers.filter((user) => !selectedUsers.includes(user))
-    );
-    setSelectedUsers([]);
-  };
-
-  const handleAddUser = () => {
-    // TODO: Add new user
-  };
-
-  const filteredUsers = users.filter((user) =>
-    user.username.toLowerCase().includes(search.toLowerCase())
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'ID',
+        accessor: 'id',
+        Cell: ({ row }) => (
+          <input
+            type="checkbox"
+            checked={selectedRows.some(
+              (banner) => banner.id === row.original.id
+            )}
+            onChange={(e) => {
+              const checked = e.target.checked;
+              setSelectedRows((selectedRows) => {
+                if (checked) {
+                  return [...selectedRows, row.original];
+                } else {
+                  return selectedRows.filter(
+                    (banner) => banner.id !== row.original.id
+                  );
+                }
+              });
+            }}
+          />
+        ),
+      },
+      { Header: 'Name', accessor: 'first_name' },
+      { Header: 'Name', accessor: 'last_name' },
+      { Header: 'Email', accessor: 'email' },
+      { Header: 'Phone', accessor: 'phone_number' },
+      { Header: 'Country', accessor: 'country_of_residence' },
+      { Header: 'Adventure', accessor: 'adventure' },
+      { Header: 'Days', accessor: 'tour_duration_in_days' },
+      { Header: 'Budget', accessor: 'budget' },
+      { Header: 'Month', accessor: 'when_to_travel' },
+      { Header: 'People', accessor: 'how_many_travelers' },
+      { Header: 'Adults', accessor: 'no_of_adults' },
+      { Header: 'Children', accessor: 'no_of_children' },
+      { Header: 'Extra Info', accessor: 'extra_info' }
+    ],
+    [selectedRows]
   );
+
+  // Define table instance with search and pagination functionalities
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 10 },
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    previousPage,
+    nextPage,
+    canPreviousPage,
+    canNextPage,
+  } = tableInstance;
+
+  const handleDelete = () => {
+    Promise.all(
+      selectedRows.map((enquiry) =>
+        fetch(`http://localhost:3000/planningforms/${enquiry.id}`, {
+          method: 'DELETE',
+        }).then((response) => {
+          if (response.ok) {
+            return response.json().then(() => {
+              setData(
+                data.filter(
+                  (enquiry) =>
+                    !selectedRows.some(
+                      (selectedAdmin) => selectedAdmin.id === enquiry.id
+                    )
+                )
+              );
+              setSelectedRows([]);
+              notifySuccess();
+            });
+          } else {
+            setSelectedRows([]);
+            notifyError();
+          }
+        })
+      )
+    ).catch((error) => console.error(error));
+  };
+
+  const notifySuccess = () =>
+    toast.success('Entry(s) deleted succesfully', {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+      style: {
+        backgroundColor: '#FFCE3C',
+        color: '#000',
+      },
+    });
+
+  const notifyError = () =>
+    toast.error('Failed to delete Entry(s).Try again.', {
+      theme: 'colored',
+    });
 
   return (
     <div className="px-4 py-8 w-full ">
-      <h1 className="text-2xl font-bold mb-4">Plannings</h1>
-      <div className="mt-2 grid grid-cols-1 lg:grid-cols-2">
-        <div className="flex items-center mb-2">
-          <input
-            type="text"
-            className="border p-2 rounded-md w-full sm:w-56"
-            placeholder="Search..."
-            value={search}
-            onChange={handleSearch}
-          />
-        </div>
-
-        <div className="sm:mt-2">
-          <button
-            className={`px-5 py-3 mr-2 mb-2 ${
-              selectedUsers.length > 0 ? 'bg-red-500' : 'bg-yellow-400'
-            } text-white rounded-lg`}
-            onClick={handleBulkDelete}
-            disabled={selectedUsers.length === 0}
-          >
-            Delete
-          </button>
-          <button
-            className="bg-yellow-400 text-white px-5 py-3   rounded-lg hover:bg-yellow-600"
-            onClick={handleAddUser}
-          >
-            Add user
-          </button>
-        </div>
-      </div>
-
-      <div className="relative shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left   table-auto">
-          <thead className="text-xs  uppercase bg-gray-50">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                ACTION
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">EMAIL</div>
-              </th>
-
-              <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">ROLE</div>
-              </th>
+      <ToastContainer />
+      <h1 className="text-2xl font-bold mb-4">Adventure Planning</h1>
+      <p>This category represents clients who are interested in going on an adventure but need help deciding where to start and which adventure to choose (such as a safari or climbing). Your role is to reach out to these clients, provide them with information, and help them plan out their trip. Offer advice on what adventure would best suit their needs. Remember to delete the message from the database after replying to avoid clutter. </p>
+      <input
+        type="text"
+        className="border p-2 rounded-md w-2/5 mt-2"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        placeholder="Search by name, email or message"
+      /> 
+      
+   <div className='overflow-x-auto'>
+   <table
+        {...getTableProps()}
+        className="w-full  shadow-md sm:rounded-lg text-left  mt-4"
+      >
+        <thead className="bg-gray-50">
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  key={column.id}
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  className={`px-3 py-3 ${
+                    column.isSorted
+                      ? column.isSortedDesc
+                        ? 'desc'
+                        : 'asc'
+                      : ''
+                  }`}
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
             </tr>
-          </thead>
-          <tbody className="divide-y divide-white">
-            {filteredUsers.map((user) => (
-              <tr key={user.id}>
-                <td className="px-6 py-4 whitespace-nowrap ">
-                  <div className="flex items-center">
-                    <div className="flex items-center">
-                      <input
-                        type="checkbox"
-                        name="selectedUsers"
-                        value={user.id}
-                        onChange={(event) => handleCheckboxChange(event, user)}
-                      />
-                    </div>
-                  </div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="flex items-center">
-                    <div>
-                      <div>{user.email}</div>
-                    </div>
-                  </div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="flex items-center">
-                    <div>{user.role}</div>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()} className="divide-y divide-white">
+          {page
+            .filter((row) => {
+              const { name, email, message } = row.values;
+              const searchRegex = new RegExp(searchValue, 'i');
+              return (
+                searchRegex.test(name) ||
+                searchRegex.test(email) ||
+                searchRegex.test(message)
+              );
+            })
+            .map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()} key={row.id}>
+                  {row.cells.map((cell) => (
+                    <td className="p-3 " key={cell.id} {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+   </div>
+      <div className="inline-flex  mt-2">
+        <button
+          className="bg-yellow-300 hover:bg-yellow-400 text-gray-800 font-bold py-2 px-4  "
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+        >
+          Prev
+        </button>
+        <button
+          className="bg-yellow-300 hover:bg-yellow-400 text-gray-800 font-bold py-2 px-4 "
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+        >
+          Next
+        </button>
+        <button
+          className={`px-4 py-2  ${
+            selectedRows.length > 0 ? 'bg-red-500' : 'bg-yellow-300'
+          } text-gray-800 `}
+          disabled={selectedRows.length === 0}
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
-};
-
-export default Plannings;
+}
