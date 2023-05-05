af89af66fa3f31cc6357eb9a7e6d5ad58fc2948c
queenslay jema <queenslayjema@gmail.com>
2023-05-05 07:55:50 +0300

    Fix:cultural tour card dimensions

--- src/components/CulturalTourCard.js
+++ src/components/CulturalTourCard.js
@@ -4,24 +4,27 @@ import Image from 'next/image';
 
 function CulturalTourCard({ tour }) {
   return (
-    <div className="sm:mt-10 md:mt-5 max-w-lg">
+    <div>
       <Link href={`/cultural-tours/${generateSlug(tour.name)}`}>
-        <div className="rounded overflow-hidden shadow-lg">
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">
-              {tour.name.toUpperCase()}
+        <div className="relative card overflow-hidden rounded-lg shadow-md hover:shadow-lg transform transition-transform hover:-translate-y-1 cursor-pointer h-80">
+          <Image
+            src="/culturalcard.jpg"
+            className="card__image w-full h-full object-cover"
+            height={500}
+            width={500}
+            alt={tour.name}
+          />
+          <div className="absolute inset-0 flex  items-center justify-center  text-left">
+            <div className="text-white p-4">
+              <h3 className="text-2xl font-bold mt-4">{tour.name}</h3>
+              <div className="absolute bottom-0 left-0 px-2 bg-yellow-300">
+                <div className="text-lg font-medium text-black">From $130</div>
+              </div>
             </div>
-            <Image
-              src="/culturalcard.jpg"
-              height={600}
-              width={1000}
-              alt={tour.name}
-              className="rounded-xl h-full"
-            />
           </div>
         </div>
       </Link>
-    </div>
+  </div>
   );
 }
 
