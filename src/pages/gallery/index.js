66317665586cf23bbae26f9bc802286cb25c7fb3
queenslay jema <queenslayjema@gmail.com>
2023-05-09 01:04:12 +0300

    Feat:modify gallery to include new images

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -13,6 +13,141 @@ const Gallery = () => {
           <div className="grid-cols-4  lg:space-y-0 grid gap-3 grid-rows-3 pb-10">
             <div className="w-full col-span-2 row-span-2 rounded">
               <Image
+                src="/kilimanjaropeak.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full">
+              <Image
+                src="/lauwo1.JPG"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full ">
+              <Image
+                src="/mountainte.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full col-span-2 row-span-2 rounded">
+              <Image
+                src="/trekkingbreakfast.JPG"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full">
+              <Image
+                src="/lauwohike.jpeg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full ">
+              <Image
+                src="/lauwoteam.jpeg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full col-span-2 row-span-2 rounded">
+              <Image
+                src="/kilimanjarosunset.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full">
+              <Image
+                src="/lemoshoclimb.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full ">
+              <Image
+                src="/hikers.jpeg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full col-span-2 row-span-2 rounded">
+              <Image
+                src="/lauwotourbus.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full">
+              <Image
+                src="/lauwoclimbers.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full ">
+              <Image
+                src="/kilimanjarofullmoon.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full col-span-2 row-span-2 rounded">
+              <Image
+                src="/womenkilimanjaro.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full">
+              <Image
+                src="/fullmoonclimb.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full ">
+              <Image
+                src="/uhurupeak.jpg"
+                alt="image"
+                width={500}
+                height={500}
+                className="min-w-full min-h-full object-cover"
+              />
+            </div>
+            <div className="w-full col-span-2 row-span-2 rounded">
+              <Image
                 src="/g-8.jpg"
                 alt="image"
                 width={500}
