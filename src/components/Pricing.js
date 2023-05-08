984085cb4182a30c5e67ea9e412d3f08d914e549
queenslay jema <queenslayjema@gmail.com>
2023-05-09 02:25:54 +0300

    Refactor:remove uused hooks

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-key */
 import React from 'react';
-import { useState, useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useTable } from 'react-table';
 export default function Pricing() {
   const data = [
