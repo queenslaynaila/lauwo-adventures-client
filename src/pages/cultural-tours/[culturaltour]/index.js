876c01d7cc945abaf89ade2d5065a05cd68d45f9
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-05-08 08:35:07 +0300

    updating pricing in the cultural tours

--- src/pages/cultural-tours/[culturaltour]/index.js
+++ src/pages/cultural-tours/[culturaltour]/index.js
@@ -93,7 +93,7 @@ function Tour({ culturalTour }) {
                 ))}
             </ul>
             <h2 className="text-2xl font-bold mb-2 text-gray-900">
-              Price: ${tour.price}
+              Price: $130
             </h2>
           </div>
 
