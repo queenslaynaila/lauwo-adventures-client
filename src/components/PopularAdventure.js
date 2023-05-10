1a6b6e58566160e72d5b149330dda4d74a1a7058
queenslay jema <queenslayjema@gmail.com>
2023-05-10 17:12:16 +0300

    Feat:replace with service cards

--- src/components/PopularAdventure.js
+++ src/components/PopularAdventure.js
@@ -1,8 +1,42 @@
-import Image from 'next/image';
+ 
 import { AiOutlineCompass } from 'react-icons/ai';
+import Link from 'next/link'
 const PopularAdventure = () => {
+  const services = [
+    {
+      title: 'Climbing Expeditions',
+      description:
+        'Embark on a challenging and rewarding journey to conquer the majestic peaks of Tanzania.',
+      image: '/kilimanjaro.jpg',
+      url: '/mountain-trekking/mount-kilimanjaro',
+    },
+    {
+      title: 'Safari Tours',
+      description:
+        'Experience the thrill of exploring Tanzania’s wildlife in their natural habitats with our expert guides.',
+      image: '/safari.jpg',
+      url: '/safari-packages',
+    },
+    {
+      title: 'Day Trips',
+      description:
+        'Discover the beauty of Tanzania on our day trips to stunning locations and cultural landmarks.',
+      image: '/daytrip.jpg',
+      url: '/daytrip-packages',
+    },
+    {
+      title: 'Cultural Tours',
+      description: 'Immerse yourself in the rich cultural heritage of Tanzania with our guided cultural tours.',
+      image: '/culturaltour.jpg',
+      url: '/culturaltour-packages',
+    },
+    
+    
+  ];
   return (
-    <section className="font-poly lg:mt-4 sm:-mt-24 mt-96 h-screen ">
+   
+    <>
+    <section className="bg-gray-100 py-12 font-poly lg:mt-4 mt-96">
       <div className="mx-50">
         <div className="flex justify-center items-center">
           <div className="border-t border-black w-1/3"></div>
@@ -12,51 +46,32 @@ const PopularAdventure = () => {
           <div className="border-t border-black w-1/3"></div>
         </div>
       </div>
-      <h1 className="font-bold text-center my-2 text-lg lg:text-3xl">
-        Popular Adventures
-      </h1>
-      <div className="grid grid-cols-3 sm:gap-x-6 gap-x-3 mx-4 sm:mx-20 mt-8">
-        <div className=" rounded-lg h-32 sm:h-60 md:h-50">
-          <Image
-            src="/safari-1.jpg"
-            alt="nnn"
-            height={500}
-            width={500}
-            className="w-full  sm:h-[400px] h-[200px] object-cover  rounded-lg"
-          />
-          <h3 className="sm:text-sm lg:text-2xl font-bold mb-3 mt-4">
-            Serengeti
-          </h3>
-        </div>
-
-        <div className="rounded-lg sm:h-60 md:h-50">
-          <Image
-            src="/kilimanjaro.jpg"
-            alt="nnn"
-            priority={true}
-            height={500}
-            width={500}
-            className="w-full sm:h-[400px] h-[200px]  object-cover  rounded-lg"
-          />
-          <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3 mt-4">
-            Kilimanjaro
-          </h3>
-        </div>
-
-        <div className=" rounded-lg h-32 sm:h-60 md:h-50">
-          <Image
-            src="/tarangire.jpg"
-            alt="nnn"
-            height={500}
-            width={500}
-            className="w-full  sm:h-[400px] h-[200px]  object-cover  rounded-lg"
-          />
-          <h3 className="sm:text-sm lg:text-2xl  font-bold mb-3 mt-4">
-            Tarangire
-          </h3>
+  
+      <div className="container mx-auto px-4">
+        <h1 className="font-bold text-center my-4 text-lg lg:text-3xl">
+          Come Experience Tanzania With Us
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+          {services.map((service, index) => (
+           <Link  key={index} href={service.url}>
+           <div
+             
+              className="bg-white rounded-lg shadow-md flex flex-col"
+            >
+              <div className="h-64 w-full bg-cover rounded-t-lg mb-4" style={{ backgroundImage: `url(${service.image})` }}></div>
+              <div className='p-4'>
+                <h3 className="text-xl font-semibold mb-2">{service.title}</h3>
+                <p className="text-gray-600 ">{service.description}</p>
+              </div>
+             
+            </div>
+           </Link>
+          ))}
         </div>
       </div>
     </section>
+  </>
+  
   );
 };
 
