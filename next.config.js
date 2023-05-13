23cfe191b8b4ed6188f9a8034d77c850d96b8533
queenslay jema <queenslayjema@gmail.com>
2023-05-13 17:20:58 +0300

    Feat:ebpack plugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-const nextConfig = {
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: [
@@ -16,6 +18,18 @@ const nextConfig = {
     ],
   },
   distDir: 'build',
-};
+  webpack: (config, { isServer }) => {
+    // Only apply the plugin on the server-build target
+    if (isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            { from: './_redirects', to: './' }
+          ],
+        })
+      );
+    }
 
-module.exports = nextConfig;
+    return config;
+  },
+};
