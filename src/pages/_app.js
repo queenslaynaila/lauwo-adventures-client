c129818d0d8abcbd1c34195c3a2d48c9ed67db09
queenslay jema <queenslayjema@gmail.com>
2023-05-15 09:53:56 +0300

    Add vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import MainComponentLayout from '../components/MainComponentLayout';
 
 export default function App({ Component, pageProps }) {
   return (
     <MainComponentLayout>
-      <Component {...pageProps} />
+      <Component {...pageProps}  />
+      <Analytics />
     </MainComponentLayout>
   );
 }
