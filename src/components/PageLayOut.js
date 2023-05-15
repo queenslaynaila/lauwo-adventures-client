d8e3a48b0ce8d441ac40e6f7911cd35d655f47dc
queenslay jema <queenslayjema@gmail.com>
2023-05-15 20:54:16 +0300

    Fix:remove head tag

--- src/components/PageLayOut.js
+++ src/components/PageLayOut.js
@@ -5,65 +5,13 @@ import PopularAdventure from './PopularAdventure';
 import InquiryForm from './InquiryForm';
 import SocialsButtons from './SocialsButtons';
 import FixedBanner from './FixedBanner';
-import Head from 'next/head';
+ 
 import Why from './Why';
 import TourGroups from './TourGroups';
 const PageLayOut = () => {
   return (
     <div>
-      <Head>
-        <title>
-          Lauwo Adventures | Mountain Climbing, Safaris, Camping, Cultural Tours
-        </title>
-        <meta
-          name="description"
-          content="Lauwo Adventures offers unforgettable mountain climbing experiences on Mt. Kilimanjaro, Mt. Meru, Usambara Mountains, and Oldonyo Lengai. Explore Tanzania with our diverse range of safaris, camping tours, cultural tours, and day trips."
-        />
-
-        <meta
-          property="og:title"
-          content="Lauwo Adventures | Mountain Climbing, Safaris,  Safaris, Cultural Tours, HoneyMoon Safaris,Day Trips"
-        />
-        <meta
-          property="og:description"
-          content="Lauwo Adventures offers unforgettable mountain climbing experiences on Mt. Kilimanjaro, Mt. Meru, Usambara Mountains, and Oldonyo Lengai. Explore Tanzania with our diverse range of safaris, camping tours, cultural tours, and day trips.We have safaris from as short as 2 days to as long as 10 days."
-        />
-        <meta
-          property="og:image"
-          content="https://media-cdn.tripadvisor.com/media/attractions-splice-spp-360x240/09/4a/80/8c.jpg"
-        />
-        <meta
-          property="og:url"
-          content="https://lauwo-adventures-client.vercel.app/"
-        />
-        <meta property="og:type" content="website" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="Lauwo Adventures | Mountain Climbing, Safaris, Camping, Cultural Tours"
-        />
-        <meta
-          name="twitter:description"
-          content="Lauwo Adventures offers unforgettable mountain climbing experiences on Mt. Kilimanjaro, Mt. Meru, Usambara Mountains, and Oldonyo Lengai. Explore Tanzania with our diverse range of safaris, camping tours, cultural tours, and day trips."
-        />
-        <meta
-          name="twitter:image"
-          content="https://media-cdn.tripadvisor.com/media/attractions-splice-spp-360x240/09/4a/80/8c.jpg"
-        />
-
-        <meta
-          name="keywords"
-          content="Lauwo Adventures, mountain climbing, safaris, camping tours, cultural tours, Tanzania, Mt. Kilimanjaro, Mt. Meru, Usambara Mountains, Oldonyo Lengai"
-        />
-        <meta name="author" content=" Lauwo adventures" />
-        <meta name="google" content="translate" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="google-site-verification"
-          content="hKFgedHOB3VpAnDkoVs-Fm7QCpHkhAh6KaHKxbMVra8"
-        />
-      </Head>
+    
 
       <Header />
       <FixedBanner />
