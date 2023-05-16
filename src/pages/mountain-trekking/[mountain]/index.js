bad12653e563f21642da216af594db9f1cd997b8
queenslay jema <queenslayjema@gmail.com>
2023-05-17 00:23:02 +0300

    Feat:change searchbar

--- src/pages/mountain-trekking/[mountain]/index.js
+++ src/pages/mountain-trekking/[mountain]/index.js
@@ -460,15 +460,43 @@ export default function Mountain({ mountain, faqs }) {
                     ALL ROUTE PACKAGES
                   </h1>
                 </div>
-                <div className="flex justify-center item-center">
-                  {' '}
-                  <input
-                    type="text"
-                    placeholder="Search by route name or  days in numbers."
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    className="p-2 mb-4 border border-black rounded  sm:w-72 w-40"
-                  />
+                <div>
+                  <form className="mx-6">
+                    <label
+                      for="default-search"
+                      class="mb-2 text-sm font-medium text-gray-900 sr-only "
+                    >
+                      Search
+                    </label>
+                    <div class="relative">
+                      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg
+                          aria-hidden="true"
+                          class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                          ></path>
+                        </svg>
+                      </div>
+                      <input
+                        type="search"
+                        id="default-search"
+                        class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg        "
+                        placeholder="Search by route name or route duration in numerics."
+                        required
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                      />
+                    </div>
+                  </form>
                 </div>
 
                 <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mx-6">
