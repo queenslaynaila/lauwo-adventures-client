c6b04784e4c15cb004400247133e5c3d0245cb94
queenslay jema <queenslayjema@gmail.com>
2023-05-17 03:03:56 +0300

    Feat:remove banner

--- src/components/PageLayOut.js
+++ src/components/PageLayOut.js
@@ -4,17 +4,13 @@ import Blogs from './Blogs';
 import PopularAdventure from './PopularAdventure';
 import InquiryForm from './InquiryForm';
 import SocialsButtons from './SocialsButtons';
-import FixedBanner from './FixedBanner';
-
 import Why from './Why';
 import TourGroups from './TourGroups';
 const PageLayOut = () => {
   return (
     <div>
       <Header />
-      <FixedBanner />
       <About />
-
       <Why />
       <PopularAdventure />
       <TourGroups />
